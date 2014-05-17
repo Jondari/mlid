99073ef62ab45828a7546d7a2dd34a489be63e3f
Giovanni <liaison.giovanni@free.fr>
2014-05-17 14:54:58 +0200

    PreprocessAppli
    Modification du programme afin qu'il prenne en argument des dossiers et
    applique les filtres sur les éléments textes du dossier source et mette
    les fichiers traités dans le dossier destination.

--- Lucene/src/fr/inrialpes/exmo/mlid/PreprocessAppli.java
+++ Lucene/src/fr/inrialpes/exmo/mlid/PreprocessAppli.java
@@ -1,5 +1,7 @@
 package fr.inrialpes.exmo.mlid;
 
+import java.util.List;
+
 import fr.inrialpes.exmo.mlid.preprocess.LowerCase;
 import fr.inrialpes.exmo.mlid.preprocess.NGrams;
 import fr.inrialpes.exmo.mlid.preprocess.PreprocessFilter;
@@ -16,7 +18,7 @@ import fr.inrialpes.exmo.mlid.util.FileUtil;
  * Pour spécifier la langue entré fr, en, ru ou ch. 
  * Pour spécifier le filtre entré : 
  * - Stop pour le StopWord 
- * - Stem pour le Stemming 
+ * - Stem pour le Stemming
  * - Token pour la Tokenization 
  * - Low pour le LowerCase 
  * - NGr pour le NGrams 
@@ -36,11 +38,11 @@ public class PreprocessAppli {
 		// si on a pas précisé d'argument au lancement de l'application
 		// on effectue les modification manuellement
 
-		// fichier à traiter
-		String filePath;
+		// dossier des fichiers à traiter
+		String dirPath;
 
-		// fichier résultat
-		String filePathDest;
+		// dossier contenant les fichiers résultats
+		String dirPathDest;
 
 		// filtre à appliquer
 		String cmd;
@@ -56,21 +58,21 @@ public class PreprocessAppli {
 		if (args.length == 0) {
 			System.out
 					.println("Pas d'argument! modifié les paramètres manuellement!");
-			filePath = "src/english_stopword.txt";
-			filePathDest = "src/test.txt";
+			dirPath = "src/english_stopword.txt";
+			dirPathDest = "src/test.txt";
 			cmd = "Low+Stop+Stem+NGr";
 			lang = "en";
 		}
 		// sinon on effecture directement le traitement des données.
-		// args[0] correspond au fichier source
-		// args[1] correspond au fichier destination
-		// args[2] correspond au filtre à appliquer sur le fichier
+		// args[0] correspond au dossier source
+		// args[1] correspond au dossier destination
+		// args[2] correspond au filtre à appliquer sur les fichiers
 		// args[3] correspond à la langue du fichier
 		// args[4] correspond à la taille du gramme
 		else {
 			System.out.println("traitement en mode console");
-			filePath = args[0];
-			filePathDest = args[1];
+			dirPath = args[0];
+			dirPathDest = args[1];
 			cmd = args[2];
 			lang = args[3];
 			if (args.length == 5) {
@@ -78,48 +80,69 @@ public class PreprocessAppli {
 			}
 		}
 
-		// On récupère le texte du fichier
-		String tmpText = FileUtil.getText(filePath);
+		// on récupère la liste des noms des fichiers textes présents dans le
+		// dossier donné en paramètre
+		List<String> listOfFile = FileUtil.getListNameFile(dirPath);
 
-		if (cmd.equalsIgnoreCase("Low")) {
-			filter = new LowerCase(tmpText);
-		} else if (cmd.equalsIgnoreCase("Stop")) {
-			filter = new StopWord(tmpText, lang);
-		} else if (cmd.equalsIgnoreCase("Stem")) {
-			filter = new Stemming(tmpText, lang);
-		} else if (cmd.equalsIgnoreCase("Token")) {
-			filter = new Tokenization(tmpText);
-		} else if (cmd.equalsIgnoreCase("NGr")) {
-			filter = new NGrams(tmpText, n);
-		} else if (cmd.equalsIgnoreCase("Low+Token")) {
-			filter = new Tokenization(new LowerCase(tmpText));
-		} else if (cmd.equalsIgnoreCase("Low+Stop")) {
-			filter = new StopWord(new LowerCase(tmpText), lang);
-		} else if (cmd.equalsIgnoreCase("Low+Stop+Token")) {
-			filter = new Tokenization(
-					new StopWord(new LowerCase(tmpText), lang));
-		} else if (cmd.equalsIgnoreCase("Low+Stop+Stem")) {
-			filter = new Stemming(new StopWord(new LowerCase(tmpText), lang),
-					lang);
-		} else if (cmd.equalsIgnoreCase("Low+Stop+Stem+Token")) {
-			filter = new Tokenization(new Stemming(new StopWord(new LowerCase(
-					tmpText), lang), lang));
-		} else if (cmd.equalsIgnoreCase("Low+Stop+Stem+NGr")) {
-			filter = new NGrams(new Stemming(new StopWord(
-					new LowerCase(tmpText), lang), lang), n);
-		}
+		// pour chaque fichier texte du dossier on applique le filtre
+		for (String filePathTemp : listOfFile) {
+			String tmpText;
+			String fileDestTemp;
+			// on vérifie la façon dont a été ecrit le chemin afin de le
+			// complété de façon adéquate
+			if (dirPathDest.contains("/")) {
+				// On récupère le texte du fichier
+				tmpText = FileUtil.getText(dirPath + "/" + filePathTemp);
+				// On définie le fichier résultat
+				fileDestTemp = dirPathDest + "/" + "mod_" + filePathTemp;
+			} else {
+				tmpText = FileUtil.getText(dirPath + "\\" + filePathTemp);
+				System.out.println(tmpText);
+				fileDestTemp = dirPathDest + "\\" + "mod_" + filePathTemp;
+			}
 
-		// On écrit la nouvelle chaîne de caractère dans le fichier destination
-		if (cmd.contains("Token") || cmd.contains("NGr")) {
-			if (cmd.contains("Token")) {
-				FileUtil.writeText(filePathDest, filter.getList(), true);
+			if (cmd.equalsIgnoreCase("Low")) {
+				filter = new LowerCase(tmpText);
+			} else if (cmd.equalsIgnoreCase("Stop")) {
+				filter = new StopWord(tmpText, lang);
+			} else if (cmd.equalsIgnoreCase("Stem")) {
+				filter = new Stemming(tmpText, lang);
+			} else if (cmd.equalsIgnoreCase("Token")) {
+				filter = new Tokenization(tmpText);
+			} else if (cmd.equalsIgnoreCase("NGr")) {
+				filter = new NGrams(tmpText, n);
+			} else if (cmd.equalsIgnoreCase("Low+Token")) {
+				filter = new Tokenization(new LowerCase(tmpText));
+			} else if (cmd.equalsIgnoreCase("Low+Stop")) {
+				filter = new StopWord(new LowerCase(tmpText), lang);
+			} else if (cmd.equalsIgnoreCase("Low+Stop+Token")) {
+				filter = new Tokenization(new StopWord(new LowerCase(tmpText),
+						lang));
+			} else if (cmd.equalsIgnoreCase("Low+Stop+Stem")) {
+				filter = new Stemming(
+						new StopWord(new LowerCase(tmpText), lang), lang);
+			} else if (cmd.equalsIgnoreCase("Low+Stop+Stem+Token")) {
+				filter = new Tokenization(new Stemming(new StopWord(
+						new LowerCase(tmpText), lang), lang));
+			} else if (cmd.equalsIgnoreCase("Low+Stop+Stem+NGr")) {
+				filter = new NGrams(new Stemming(new StopWord(new LowerCase(
+						tmpText), lang), lang), n);
 			}
-			if (cmd.contains("NGr")) {
-				FileUtil.writeText(filePathDest, ((NGrams) filter).getCrtList(), true);
+
+			// On écrit la nouvelle chaîne de caractère dans le fichier
+			// destination
+			if (cmd.contains("Token") || cmd.contains("NGr")) {
+				if (cmd.contains("Token")) {
+					FileUtil.writeText(fileDestTemp, filter.getList(), true);
+				}
+				if (cmd.contains("NGr")) {
+					FileUtil.writeText(dirPathDest,
+							((NGrams) filter).getCrtList(), true);
+				}
+			} else {
+				System.out.println(filter.getCrtString());
+				FileUtil.writeText(fileDestTemp, filter.getCrtString());
 			}
-		} else {
-			System.out.println(filter.getCrtString());
-			FileUtil.writeText(filePathDest, filter.getCrtString()); 
 		}
 	}
 
