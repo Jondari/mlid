4d8b2cba753e37d7ac10028eeea5a3a4071c9806
Giovanni <liaison.giovanni@free.fr>
2014-06-03 17:32:08 +0200

    BabelNetAppli - Séparation de la méthode main en deux méthodes : main et getBabelID

--- Lucene/src/fr/inrialpes/exmo/mlid/BabelNetAppli.java
+++ Lucene/src/fr/inrialpes/exmo/mlid/BabelNetAppli.java
@@ -41,10 +41,6 @@ public class BabelNetAppli {
 		option.addOption(pathDest);
 		option.addOption(lang);
 
-		String noResult = "No result found";
-
-		String pathDirectoryReport = null;
-
 		if (args.length == 0) {
 			System.out
 					.println("Pas d'argument! Veuillez entrer des arguments!");
@@ -59,50 +55,76 @@ public class BabelNetAppli {
 				String pathFileS = cmd.getOptionValue("f1");
 				String pathDestS = cmd.getOptionValue("f2");
 				String langS = cmd.getOptionValue("lang");
-				if (pathDestS.contains("/")) {
-					pathDirectoryReport = pathDestS.substring(0,
-							pathDestS.lastIndexOf("/"))
-							+ "/report.txt";
-				} else {
-					pathDirectoryReport = pathDestS.substring(0,
-							pathDestS.lastIndexOf("\\"))
-							+ "\\report.txt";
-				}
 
-				// on récupère la chaine de caractère */
-				String text = FileUtil.getText(pathFileS);
-				System.out.println(text);
+				getBabelID(pathFileS, pathDestS, langS);
 
-				// on traite la chaine de charactère
-				List<String> listTerm = new StopWord(new LowerCase(text), langS)
-						.getList();
-				System.out.println(listTerm.toString());
-				// System.out.println(listTerm.toString());
-				listTerm = Comparateur.removeDuplicate(listTerm);
-				System.out.println(listTerm.toString());
-				// System.out.println("*** Après suppression doublon ***");
-				// System.out.println(listTerm.toString());
-				// récupère la liste d'id babelnet
-				List<String> idText = BabelNetService.getListBabelNetId(
-						listTerm, langS);
-				System.out.println(idText.toString());
-
-				// on rapporte dans un autre fichier les termes n'ayant pas eu
-				// d'id
-				Comparateur.reportElementNotFound(listTerm, idText,
-						pathDirectoryReport);
-
-				// suppression de ses termes de la liste
-				Comparateur.filterTerm(idText, noResult);
-
-				// on écrit la liste dans un autre fichier text
-				FileUtil.writeText(pathDestS, idText, true);
 			} catch (ParseException e) {
 				// Affichage de l'aide
 				HelpFormatter formatter = new HelpFormatter();
-                formatter.printHelp("BalbelnetAppli", option);
+				formatter.printHelp("BalbelnetAppli", option);
 			}
 		}
+	}
+
+	/**
+	 * Méthode qui retourne un fichier f2 contenant les id babelnet des mots du fichier f1
+	 * @param f1
+	 *            fichier à traiter
+	 * @param f2
+	 *            fichier résultat
+	 * @param langu
+	 *            langue du fichier à traiter
+	 */
+	private static void getBabelID(String f1, String f2, String langu) {
+		String noResult = "No result found";
+
+		/** fichier contenant les terme n'ayant pas obtenu d'id babelnet */
+		String pathDirectoryReport = null;
+
+		/** fichier à traiter */
+		String pathFileS = f1;
+		/** fichier résultat */
+		String pathDestS = f2;
+		/** langue du fichier à traiter */
+		String langS = langu;
+		if (pathDestS.contains("/")) {
+			pathDirectoryReport = pathDestS.substring(0,
+					pathDestS.lastIndexOf("/"))
+					+ "/report.txt";
+		} else {
+			pathDirectoryReport = pathDestS.substring(0,
+					pathDestS.lastIndexOf("\\"))
+					+ "\\report.txt";
+		}
+
+		// on récupère la chaine de caractère */
+		String text = FileUtil.getText(pathFileS);
+		System.out.println(text);
+
+		// on traite la chaine de charactère
+		List<String> listTerm = new StopWord(new LowerCase(text), langS)
+				.getList();
+		System.out.println(listTerm.toString());
+		// System.out.println(listTerm.toString());
+		listTerm = Comparateur.removeDuplicate(listTerm);
+		System.out.println(listTerm.toString());
+		// System.out.println("*** Après suppression doublon ***");
+		// System.out.println(listTerm.toString());
+		// récupère la liste d'id babelnet
+		List<String> idText = BabelNetService
+				.getListBabelNetId(listTerm, langS);
+		System.out.println(idText.toString());
+
+		// on rapporte dans un autre fichier les termes n'ayant pas eu
+		// d'id
+		Comparateur
+				.reportElementNotFound(listTerm, idText, pathDirectoryReport);
+
+		// suppression de ses termes de la liste
+		Comparateur.filterTerm(idText, noResult);
+
+		// on écrit la liste dans un autre fichier text
+		FileUtil.writeText(pathDestS, idText, true);
 	}
 
 }
