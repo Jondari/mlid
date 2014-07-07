da78cd2bd4929da426e3e6d5e4536ee94893a592
ismael <giovanni.ismael@inria.fr>
2014-07-07 11:37:26 +0200

    ComputeMax -Formatage du code -Ajout d'argument à la méthode compareDiffLangU
    Comparateur
    - Certaine méthode prennent maintenan les argument lang1 et lang2
    - Modification de la méthode de calcule du max
    
    Suppression de la classe ComparateurAppli

--- Lucene/src/fr/inrialpes/exmo/mlid/ComputeMax.java
+++ Lucene/src/fr/inrialpes/exmo/mlid/ComputeMax.java
@@ -19,7 +19,7 @@ import fr.inrialpes.exmo.mlid.util.FileUtil;
 import fr.inrialpes.exmo.mlid.util.ListUtil;
 
 public class ComputeMax {
-	
+
 	/**
 	 * Variable indiquant que le terme ne dispose pas d'id babelnet
 	 */
@@ -30,7 +30,7 @@ public class ComputeMax {
 
 		// Classe qui regroupe toute les options définies
 		Options option = new Options();
-		
+
 		// dossier contenant les fichiers babelnet d'une langue
 		String pathDirectoryLang1 = null;
 		Option d1 = new Option("d1", true,
@@ -46,25 +46,25 @@ public class ComputeMax {
 		// Oblige le paramètre d'être présent lors de l'exécution
 		d2.setRequired(true);
 		String d2S = null;
-		
+
 		// Langue du dossier d1
-		Option langD1 = new Option("langD1", true,"langue du dossier d1");
+		Option langD1 = new Option("langD1", true, "langue du dossier d1");
 		// Oblige le paramètre d'être présent lors de l'exécution
 		langD1.setRequired(true);
 		String langD1S = null;
 
 		// Langue du dossier d2
-		Option langD2 = new Option("langD2", true,"langue du dossier d2");
+		Option langD2 = new Option("langD2", true, "langue du dossier d2");
 		// Oblige le paramètre d'être présent lors de l'exécution
 		langD2.setRequired(true);
 		String langD2S = null;
-		
+
 		// Chemin vers le fichier de rapport
 		String pathDirectoryReport = null;
 		Option f = new Option("f", true, "Chemin ver le fichier de rapport");
 		// Oblige le paramètre d'être présent lors de l'exécution
 		f.setRequired(true);
-		
+
 		// On ajoute nos Option
 		option.addOption(d1);
 		option.addOption(langD1);
@@ -73,23 +73,25 @@ public class ComputeMax {
 		option.addOption(f);
 
 		if (args.length == 0) {
-			throw new RuntimeException("Pas d'argument! Veuillez entrer des arguments!");
+			throw new RuntimeException(
+					"Pas d'argument! Veuillez entrer des arguments!");
 		}
 		// sinon on effecture directement le traitement des données.
 		else {
 			CommandLineParser parser = new GnuParser();
 			try {
 				CommandLine cmds = parser.parse(option, args);
-				
+
 				// on récupère les valeurs associé aux options
 				pathDirectoryLang1 = cmds.getOptionValue("d1");
 				pathDirectoryLang2 = cmds.getOptionValue("d2");
 				pathDirectoryReport = cmds.getOptionValue("f");
 				langD1S = cmds.getOptionValue("langD1");
 				langD2S = cmds.getOptionValue("langD2");
-				
+
 				// on lance la comparaison
-				compute(pathDirectoryLang1,pathDirectoryLang2,langD1S,langD2S,pathDirectoryReport);
+				compute(pathDirectoryLang1, pathDirectoryLang2, langD1S,
+						langD2S, pathDirectoryReport);
 
 			} catch (ParseException e) {
 				// Affichage de l'aide
@@ -100,13 +102,15 @@ public class ComputeMax {
 	}
 
 	/**
-	 * Méthode qui compare les fichiers presents dans les dossiers d1
-	 * avec ceux du dossier d2. Les dossier doivent être de langues différentes
+	 * Méthode qui compare les fichiers presents dans les dossiers d1 avec ceux
+	 * du dossier d2. Les dossier doivent être de langues différentes
 	 * 
 	 * @param d1
-	 *            Chemin vers le premier dossier contenant les fichiers à comparer
+	 *            Chemin vers le premier dossier contenant les fichiers à
+	 *            comparer
 	 * @param d2
-	 *            Chemin vers le second dossier contenant les fichiers à comparer
+	 *            Chemin vers le second dossier contenant les fichiers à
+	 *            comparer
 	 * @param langD1
 	 *            Langue du dossier 1
 	 * @param langD2
@@ -114,66 +118,75 @@ public class ComputeMax {
 	 * @param pathReport
 	 *            Chemin vers le fichier de rapport
 	 */
-	public static void compute(String d1, String d2, String langD1, String langD2, String pathReport) {
-		// on crée une liste qui contient tout les textes des fichiers du dossier 1 
+	public static void compute(String d1, String d2, String langD1,
+			String langD2, String pathReport) {
+		// on crée une liste qui contient tout les textes des fichiers du
+		// dossier 1
 		List<String> testComp1 = FileUtil.getListOfText(d1);
-		
-		//on fait de même pour le dossier 2
+
+		// on fait de même pour le dossier 2
 		List<String> testComp2 = FileUtil.getListOfText(d2);
-		
+
 		// on vérifie que chaque dossier contient au moins un fichier
 		if (testComp1.size() < 1 || testComp2.size() < 1) {
-			throw new RuntimeException("Vos dossiers doivent contenir au moins un fichier! La comparaison n'aura pas lieu.");
+			throw new RuntimeException(
+					"Vos dossiers doivent contenir au moins un fichier! La comparaison n'aura pas lieu.");
 		} else {
 			List<List<String>> listOfList = new ArrayList<List<String>>();
 
-			//On récupère les noms de fichier du dossier 1
-			List<String> nameFile1 = FileUtil
-					.getListNameFile(d1);
+			// On récupère les noms de fichier du dossier 1
+			List<String> nameFile1 = FileUtil.getListNameFile(d1);
 			// idem pour le dossier 2
-			List<String> nameFile2 = FileUtil
-					.getListNameFile(d2);
+			List<String> nameFile2 = FileUtil.getListNameFile(d2);
 
 			computeProcess(testComp1, nameFile1, listOfList, langD1);
-			
+
 			computeProcess(testComp2, nameFile2, listOfList, langD2);
 
-			//on compare les éléments contenus dans la liste de liste
+			// on compare les éléments contenus dans la liste de liste
 			Comparateur testComp = new Comparateur(listOfList);
 			testComp.setPathReport(pathReport);
 			testComp.compareOrderedDescU();
-			
-			testComp.setPathReport(pathReport.substring(0, pathReport.length()-4) + "2.txt");
-			testComp.compareDiffLangU();
+			System.out
+					.println("*****************************************************************");
+			testComp.setPathReport(pathReport.substring(0,
+					pathReport.length() - 4) + "2.txt");
+			testComp.compareDiffLangU(langD1, langD2);
 
 		}
 	}
-	
+
 	/**
-	 * Méthode qui récupère la liste de mots associée à chacun des textes contenus dans la liste. 
-	 * Elle ajoute en tête de liste le nom du fichier du fichier correspondant et ajoute les listes de mot à la liste de listes entrée en paramètre.
+	 * Méthode qui récupère la liste de mots associée à chacun des textes
+	 * contenus dans la liste. Elle ajoute en tête de liste le nom du fichier du
+	 * fichier correspondant et ajoute les listes de mot à la liste de listes
+	 * entrée en paramètre.
+	 * 
 	 * @param list
-	 * 			liste de texte dont on souhaite récupèrer les listes de mot
+	 *            liste de texte dont on souhaite récupèrer les listes de mot
 	 * @param nameList
-	 * 			lste contenant le nom des fichiers dont son issu les textes
+	 *            lste contenant le nom des fichiers dont son issu les textes
 	 * @param listOfList
-	 * 			liste qui contiendra les listes de mots
+	 *            liste qui contiendra les listes de mots
 	 */
-	private static void computeProcess(List<String> list, List<String> nameList, List<List<String>> listOfList, String lang){
+	private static void computeProcess(List<String> list,
+			List<String> nameList, List<List<String>> listOfList, String lang) {
 		int i = 0;
 		for (String text : list) {
-			// récupération de la liste de terme 
+			// récupération de la liste de terme
 			PreprocessFilter token = new Tokenization(text);
 			List<String> listBabelId = token.getList();
 
-			// ajout du nom de fichier en tête de liste 
-			listBabelId.set(0, nameList.get(i).substring(0, nameList.get(i).length()-4) + "(" + lang + ")");
+			// ajout du nom de fichier en tête de liste
+			listBabelId.set(0,
+					nameList.get(i).substring(0, nameList.get(i).length() - 4)
+							+ "(" + lang + ")");
 
-			// ajout de l'élément à la liste de liste 
+			// ajout de l'élément à la liste de liste
 			listOfList.add(listBabelId);
 
 			i++;
 		}
 	}
-	
+
 }
