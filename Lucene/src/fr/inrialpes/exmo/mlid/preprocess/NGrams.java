a8285b89faf16369f3c018c16ba00accb3365b20
ismael <giovanni.ismael@inria.fr>
2014-07-11 11:58:44 +0200

    NGrams -Ajout d'un constructeur demandant un booléen permettant de choisir si on veut mettre des espace entre les grammes ou pas (utile pour le chinois) -Modification de la méthode process pour qu'elle fonctionne avec ce booléen

--- Lucene/src/fr/inrialpes/exmo/mlid/preprocess/NGrams.java
+++ Lucene/src/fr/inrialpes/exmo/mlid/preprocess/NGrams.java
@@ -9,14 +9,26 @@ public class NGrams extends PreprocessFilter {
 
 	public NGrams(PreprocessFilter crtfilter, int n) {
 		this.crtString = crtfilter.getCrtString();
-		this.crtList = process(crtString, n);
+		this.crtList = process(crtString, n, true);
+		// String newString = crtList.toString();
+		// crtfilter.setCrtString(newString);
+	}
+
+	public NGrams(PreprocessFilter crtfilter, int n, boolean withSpace) {
+		this.crtString = crtfilter.getCrtString();
+		this.crtList = process(crtString, n, withSpace);
 		// String newString = crtList.toString();
 		// crtfilter.setCrtString(newString);
 	}
 
 	public NGrams(String text, int n) {
 		this.crtString = text;
-		this.crtList = process(this.crtString, n);
+		this.crtList = process(this.crtString, n, true);
+	}
+
+	public NGrams(String text, int n, boolean withSpace) {
+		this.crtString = text;
+		this.crtList = process(this.crtString, n, withSpace);
 	}
 
 	@Override
@@ -27,38 +39,46 @@ public class NGrams extends PreprocessFilter {
 		return null;
 	}
 
-	public List<String> process(String text, int n) {
+	public List<String> process(String text, int n, boolean withSpace) {
 
-			String[] grams = getNgrams(text, n);
-			List<String> wordList = new ArrayList<String>();
-			for(int i=0;i<grams.length;i++) {
-					wordList.add(grams[i]);
+		String[] grams = getNgrams(text, n);
+		List<String> wordList = new ArrayList<String>();
+		for (int i = 0; i < grams.length; i++) {
+			if (withSpace) {
+				wordList.add(grams[i]);
+			} else {
+				wordList.add(grams[i].replace(" ", ""));
 			}
-			return wordList;
+
+		}
+		return wordList;
 	}
-	
+
 	/**
-	 * Méthode qui retourne un tableau de tout les ngrams de taille entré en paramètre
-	 * @param text 
-	 * 				texte devant être de plus d'un caractère
+	 * Méthode qui retourne un tableau de tout les ngrams de taille entré en
+	 * paramètre
+	 * 
+	 * @param text
+	 *            texte devant être de plus d'un caractère
 	 * @param length
-	 * 				taille du gramme
+	 *            taille du gramme
 	 * @return
 	 */
 	private String[] getNgrams(String text, int length) {
-	    String[] parts = text.split(" ");
-	    String[] result = new String[parts.length - length + 1];
-	    for(int i = 0; i < parts.length - length + 1; i++) {
-	       StringBuilder sb = new StringBuilder();
-	       for(int k = 0; k < length; k++) {
-	           if(k > 0) sb.append(' ');
-	           sb.append(parts[i+k]);
-	       }
-	       result[i] = sb.toString();
-	    }
-	    return result;
+		String[] parts = text.split(" ");
+		String[] result = new String[parts.length - length + 1];
+		for (int i = 0; i < parts.length - length + 1; i++) {
+			StringBuilder sb = new StringBuilder();
+			for (int k = 0; k < length; k++) {
+				if (k > 0)
+					sb.append(' ');
+				sb.append(parts[i + k]);
+			}
+			result[i] = sb.toString();
+		}
+		return result;
 	}
-	
+
 	/**
 	 * @return the crtList
 	 */
