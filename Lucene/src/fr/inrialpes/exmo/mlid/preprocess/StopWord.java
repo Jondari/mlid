f60b8edb3836cd44c0a4c67717b90709ab616c27
ismael <giovanni.ismael@inria.fr>
2014-07-11 11:17:57 +0200

    StopWord -Possibilité de filtrer les caractères numèriques -Ajout de la méthode removeSpace -Ajout de la méthode removeNumber

--- Lucene/src/fr/inrialpes/exmo/mlid/preprocess/StopWord.java
+++ Lucene/src/fr/inrialpes/exmo/mlid/preprocess/StopWord.java
@@ -6,6 +6,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 import java.util.StringTokenizer;
+import java.util.regex.Pattern;
 
 public class StopWord extends PreprocessFilter {
 
@@ -41,8 +42,29 @@ public class StopWord extends PreprocessFilter {
 		 * pour supprimer les mots vides révélés par la première application du
 		 * filtre
 		 */
-		if (lang.equals("fr")) {
+		if (lang.equals("fr") || lang.equals("en")) {
 			this.crtString = this.process(this.crtString, lang);
+		}
+	}
+
+	/**
+	 * effectue un filtrage des mots vides de la langue spécifiée pour le texte
+	 * entré en paramètre langue spécifiable anglais par en chinois par zh
+	 * français par fr
+	 */
+	public StopWord(String text, String lang, boolean noNumber) {
+		this.crtString = text;
+		this.crtString = this.process(this.crtString, lang);
+		/*
+		 * si chaine de caractère en langue française on réapplique le filtre
+		 * pour supprimer les mots vides révélés par la première application du
+		 * filtre
+		 */
+		if (lang.equals("fr") || lang.equals("en")) {
+			this.crtString = this.process(this.crtString, lang);
+		}
+		if (noNumber) {
+			this.crtString = this.removeNumber(this.crtString);
 		}
 	}
 
@@ -61,10 +83,34 @@ public class StopWord extends PreprocessFilter {
 		 * pour supprimer les mots vides révélés par la première application du
 		 * filtre
 		 */
-		if (lang.equals("fr")) {
+		if (lang.equals("fr") || lang.equals("en")) {
 			process(crtString, lang);
 		}
 		// crtfilter.setCrtString(newString);
+	}
+
+	/**
+	 * effectue un filtrage des mots vides de la spécifiée sur la chaine traité
+	 * par un autre filtre
+	 * 
+	 * @param crtfilter
+	 * @param lang
+	 */
+	public StopWord(PreprocessFilter crtfilter, String lang, boolean noNumber) {
+		this.crtString = crtfilter.getCrtString();
+		process(crtString, lang);
+		/*
+		 * si chaine de caractère en langue française on réapplique le filtre
+		 * pour supprimer les mots vides révélés par la première application du
+		 * filtre
+		 */
+		if (lang.equals("fr") || lang.equals("en")) {
+			process(crtString, lang);
+		}
+		// crtfilter.setCrtString(newString);
+		if (noNumber) {
+			this.crtString = this.removeNumber(this.crtString);
+		}
 	}
 
 	@Override
@@ -77,11 +123,14 @@ public class StopWord extends PreprocessFilter {
 		return crtString;
 	}
 
-	@SuppressWarnings("resource")
 	/**
-	 * Méthode qui supprime les mots vides de la langue spécifié du texte entré en paramètre
-	 * @param text texte à traiter
-	 * @param lang langue de l'anti dictionnaire
+	 * Méthode qui supprime les mots vides de la langue spécifié du texte entré
+	 * en paramètre
+	 * 
+	 * @param text
+	 *            texte à traiter
+	 * @param lang
+	 *            langue de l'anti dictionnaire
 	 * @return
 	 */
 	public String process(String text, String lang) {
@@ -98,6 +147,8 @@ public class StopWord extends PreprocessFilter {
 	/**
 	 * méthode qui récupère une liste de mot et la met dans une liste et
 	 * retourne la obtenu liste
+	 * 
+	 * @deprecated
 	 */
 	List<String> getStopWordList(String filePath) {
 		Scanner scanner;
@@ -173,7 +224,7 @@ public class StopWord extends PreprocessFilter {
 	 * spécifié.
 	 * 
 	 * @param text
-	 *            chaine de caactère à filtrer
+	 *            chaine de caractère à filtrer
 	 * @param wordToFilter
 	 *            mot à enlever
 	 * @return nouvelle chaine ne possédant plus le mot spécifié
@@ -183,26 +234,48 @@ public class StopWord extends PreprocessFilter {
 		String result = "";
 		StringTokenizer st = new StringTokenizer(text);
 		// nnombre de mot de la chaine de caractère
-		//int numToken = st.countTokens();
+		// int numToken = st.countTokens();
 		while (st.hasMoreTokens()) {
 			token = st.nextToken();
 			result += (token.equals(wordToFilter)) ? "" : token + " ";
 		}
 		return result;
 	}
-	
+
 	/**
 	 * Méthode qui élimine la ponctuation du texte entré en paramètre
+	 * 
 	 * @param text
 	 * @return
 	 */
-	private String removePunctuation(String text){
+	private String removePunctuation(String text) {
 		String tmp = text;
 		for (String stopWord : ConstantStopWord.puntuation) {
 			if (tmp.contains(stopWord)) {
 				tmp = tmp.replace(stopWord, "");
 			}
 		}
+		for (String stopWord : ConstantStopWord.puntuation_space) {
+			if (tmp.contains(stopWord)) {
+				tmp = tmp.replace(stopWord, " ");
+			}
+		}
+		return tmp;
+	}
+
+	/**
+	 * Méthode qui élimine tout les chaines numériques
+	 * 
+	 * @param text
+	 * @return
+	 */
+	private String removeNumber(String text) {
+		String tmp = text.replaceAll("[0-9]+?", "");
+
+		// s'il y a plus d'un espace, on remplace ce nombre d'espace par un
+		// unique espace
+		Pattern p = Pattern.compile("\\s{2,}");
+		tmp = p.matcher(tmp).replaceAll("\\s");
 		return tmp;
 	}
 
